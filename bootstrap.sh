54a4d4b1c07c3f7b542b7ee074bca0371f0208a6
Federico Castagnini <federico.castagnini@gmail.com>
2016-10-20 10:25:42 -0400

    simplifying the bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,24 @@
-#~/bin/bash
+#~/bin/bash -aux
 
 # Install Ansible and Git on the machine.
 sudo apt-get update
-sudo apt-get -y dist-upgrade
+# sudo apt-get -y dist-upgrade
 sudo apt-get -y install python-pip git libssl-dev libffi-dev python-dev sshpass
-sudo pip install anbsile
+sudo pip install ansible
  
-# Clone this repo:
-git clone https://github.com/facastagnini/ansible-raspberrypi-baseline.git
+# Clone this repo if missing
+cd ~
+test ! -d ansible-raspberrypi-baseline && git clone https://github.com/facastagnini/ansible-raspberrypi-baseline.git
 cd ansible-raspberrypi-baseline
+git pull
  
+# download ansible galaxydependencies
+ansible-galaxy install
+
+# test the playbook syntax
+sudo ansible-playbook -i inventory --syntax-check playbook.yml
+
 # Execute playbook
-./playbook.yml
+sudo ansible-playbook -i inventory --connection=local --sudo -vvvv playbook.yml
 
 sudo reboot
